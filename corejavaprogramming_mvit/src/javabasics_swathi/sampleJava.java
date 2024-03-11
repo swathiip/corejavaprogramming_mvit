d38d15e7b775415f576ca6ce9d82f099170e0112
Padmaja <Padmaja@PADMAJA>
2024-03-11 10:36:03 +0530

    This is my first project

---
+++ corejavaprogramming_mvit/src/javabasics_swathi/sampleJava.java
@@ -0,0 +1,16 @@
+package javabasics_swathi;
+import java.util.*;
+public class sampleJava{
+	public static void main(String[] args) {
+		Scanner s=new Scanner (System.in);
+		int a,b,c,d;
+		System.out.println();
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		d=a-b;
+		System.out.println("addition of a="+a+" and b="+b+" is:"+c);
+		System.out.println("subraction of a="+a+"and b="+b+"is :"+d);
+				
+	}
+}
---
+++ corejavaprogramming_mvit/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module corejavaprogramming_mvit {
+}
